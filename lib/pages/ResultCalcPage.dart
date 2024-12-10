8b7d34c97c2b7e50fba305d1f7e1dc5a86774917
Ebraali2003 <ebraali2003@gmail.com>
2024-12-10 19:13:43 +0300

    add

--- lib/pages/ResultCalcPage.dart
+++ lib/pages/ResultCalcPage.dart
@@ -1,18 +1,50 @@
+// import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
+// class ResultCalcPage extends StatelessWidget {
+//   final int score; // Score obtained by the user
+//   final int totalQuestions; // Total number of questions
 
-class Resultcalcpage extends StatefulWidget {
-  const Resultcalcpage({super.key});
+//   const ResultCalcPage({
+//     Key? key,
+//     required this.score,
+//     required this.totalQuestions,
+//   }) : super(key: key);
 
-  @override
-  State<Resultcalcpage> createState() => _Resultcalcpage();
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     // Calculate the percentage score
+//     double percentage = (score / totalQuestions) * 100;
 
-class _Resultcalcpage
- extends State<Resultcalcpage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp();
-  }
-
-}
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Quiz Results'),
+//       ),
+//       body: Container(
+//         padding: EdgeInsets.all(20),
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 'Your Score: $score/$totalQuestions',
+//                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(height: 20),
+//               Text(
+//                 'Percentage: ${percentage.toStringAsFixed(1)}%',
+//                 style: TextStyle(fontSize: 20),
+//               ),
+//               SizedBox(height: 40),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.of(context).pop(); // Go back to the quiz page
+//                 },
+//                 child: Text('Retake Quiz'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
